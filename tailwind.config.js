d2d9fd1a5af97957585c29952ed09f0d2a0945fb
Kunal17717 <imperialmotivates@gmail.com>
2026-04-04 18:20:27 +0530

    Fix Tailwind CSS production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,7 +64,8 @@ export default {
       fontFamily: {
         "headline": ["Inter"],
         "body": ["Inter"],
-        "label": ["Inter"]
+        "label": ["Inter"],
+        "inter": ["Inter", "sans-serif"]
       }
     },
   },
